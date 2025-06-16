8a50f059ed4e9690f9deede19b0d61cdc6c9dc50
Rifyal05 <rifyalaidil05@gmail.com>
2025-06-16 12:24:25 +0700

    UPDATE STRUKTUR PROYEK

--- src/main/java/com/mycompany/pelacak/langganan/digital/app/App.java
+++ src/main/java/com/mycompany/pelacak/langganan/digital/app/App.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.pelacak.langganan.digital;
+package com.mycompany.pelacak.langganan.digital.app.db;
 
 /**
  *
  * @author rifial
  */
-public class PelacakLanggananDigital {
+public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
---
+++ src/main/java/com/mycompany/pelacak/langganan/digital/db/DatabaseConnection.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.DatabaseConnection to edit this template
+ */
+package com.mycompany.pelacak.langganan.digital.app.db;
+
+/**
+ *
+ * @author rifial
+ */
+public class DatabaseConnection {
+    
+}
