5892e5d841b5aae0b4b82f6f7d2c245f21642255
Rifyal05 <rifyalaidil05@gmail.com>
2025-06-16 11:45:31 +0700

    INISIASI PEOJECT

---
+++ src/main/java/com/mycompany/pelacak/langganan/digital/PelacakLanggananDigital.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pelacak.langganan.digital;
+
+/**
+ *
+ * @author rifial
+ */
+public class PelacakLanggananDigital {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
